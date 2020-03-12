59f7f3bd9e024010e841fa0400f64532c57b98b6
hsynkmr <57600552+hsynkmr@users.noreply.github.com>
2020-03-11 21:07:51 -0400

    Update SecondClass.java

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -1,5 +1,7 @@
 public class SecondClass {
     public static void main(String[] args) {
         System.out.println("brave new world");
+        
+       // I Love JAVA, Selenium, Automation, Git and GitHub
     }
 }
