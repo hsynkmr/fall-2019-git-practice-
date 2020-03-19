358591ac3230eb32555c65086650b7cbfeae1fe0
HsynKmr <huseyinkumru89@gmail.com>
2020-03-18 21:21:07 -0400

    SecondClass

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -1,7 +1,4 @@
 public class SecondClass {
-    public static void main(String[] args) {
-        System.out.println("brave new world");
-        
-       // I Love JAVA, Selenium, Automation, Git and GitHub
+    //change one
     }
-}
+
