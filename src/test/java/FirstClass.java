fc344b3786d262fca64d15b1052e779442dd6c50
hsynkmr <57600552+hsynkmr@users.noreply.github.com>
2020-03-25 19:55:09 -0400

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,7 +1,7 @@
 public class FirstClass {
     public static void main(String[] args) {
 
-        System.out.println("Hello world");
+        System.out.println("Hello world"); // comment
         System.out.println("someone");
         System.out.println("change one");
         System.out.println("is it gonna change?");
